8679b3a7963a5756ddb842aba73b94fcecaa3d7b
nembo93 <francesco.c193@libero.it>
2022-11-30 14:56:59 +0100

    Aggiunge components header e main

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
-import { mainApp } from "./components/mainApp.vue";
+import headerApp from "./components/headerApp.vue";
+import mainApp from "./components/mainApp.vue";
 
 export default{
   components: {
+    headerApp,
     mainApp
   },
 };
 </script>
 
 <template>
+  <headerApp />
   <mainApp />
   <h1>ciao</h1>
 </template>
---
+++ src/components/headerApp.vue
@@ -0,0 +1,12 @@
+<script>
+export default{
+    name: "headerApp",
+};
+</script>
+
+<template>
+    
+</template>
+
+<style>
+</style>
