7be6be7f6c3fc8ae17bd742f117c361c6e6b02d4
nembo93 <francesco.c193@libero.it>
2022-12-01 12:43:36 +0100

    Aggiunge ad funzione prova possibilità di stampare in console risultati di ricerca API in base a variabile searchText

--- src/components/headerApp.vue
+++ src/components/headerApp.vue
@@ -15,18 +15,36 @@ export default{
     methods:{
     prova: function(){
         console.log(store.searchText)
-        console.log(store.characters)
-        },
-    },
 
-    mounted() {
         axios
-            .get('https://www.breakingbadapi.com/api/characters')
+            .get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: "071cb57202acff2ed1f9abd198110e68",
+                    query: store.searchText,
+                    language: "it-IT",
+                },
+            })
             .then((response) => {
             this.store.characters = response.data;
             console.log(store.characters)
             })
         }
+    },
+
+    // mounted() {
+    //     axios
+    //         .get('https://api.themoviedb.org/3/search/movie', {
+    //             params: {
+    //                 api_key: "071cb57202acff2ed1f9abd198110e68",
+    //                 query: store.searchText,
+    //                 language: "it-IT",
+    //             },
+    //         })
+    //         .then((response) => {
+    //         this.store.characters = response.data;
+    //         console.log(store.characters)
+    //         })
+    //     }
     
 };
 </script>
